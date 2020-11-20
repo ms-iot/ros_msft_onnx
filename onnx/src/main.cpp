312a9e8b8128152b89506471a2fa406787a8ffef
Amel Hassan <amhassa@microsoft.com>
2020-11-19 21:05:49 -0800

    Added node shutdown

--- onnx/src/main.cpp
+++ onnx/src/main.cpp
@@ -15,6 +15,7 @@ int main(int argc, char **argv)
     if(tracker.init(node))
     {
         rclcpp::spin(node);
+        RCLCPP_INFO(node->get_logger(), "Shutting down onnx_processor node");
         rclcpp::shutdown();
         return 0;
     }
